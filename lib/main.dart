ee4987577613a2d6613e45d8709e098a7891c3df
EngYoussef1 <101152968+EngYoussef1@users.noreply.github.com>
2024-03-06 22:18:11 +0200

    main pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo/view/home.dart';
+import 'package:todo/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/view/done.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class donePage extends StatelessWidget {
+  const donePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("donePage"),
+    );
+  }
+}
